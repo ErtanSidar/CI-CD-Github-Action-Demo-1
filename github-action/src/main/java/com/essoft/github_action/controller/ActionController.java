5d89d96e58a62f530cbea25f607c9f5626d83b7b
ErtanSidar <essoft3@gmail.com>
2024-11-02 16:57:19 +0300

    init

---
+++ github-action/src/main/java/com/essoft/github_action/controller/ActionController.java
@@ -0,0 +1,18 @@
+package com.essoft.github_action.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/action")
+public class ActionController {
+
+    @GetMapping
+    public ResponseEntity<String> getAction() {
+        return new ResponseEntity<>("Github Action Version:1.0.0", HttpStatus.OK);
+    }
+
+}
